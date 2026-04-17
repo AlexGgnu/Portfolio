d04edfed70b16791b094541e171e82990e04831d
Alexis Gagneau <alexis.gagneau@icloud.com>
2026-04-17 14:03:23 +0000

    feat: Adding cross icon and animation on the menu toogle button

--- public/scripts/menu-toggle.js
+++ public/scripts/menu-toggle.js
@@ -1,7 +1,13 @@
 const menuToggleButton = document.getElementById('menu-toggle');
 const header = document.querySelector('header');
+let hasToggled = false;
 
 menuToggleButton.addEventListener('click', () => {
     const menuState = header.getAttribute('data-menu-state');
-    header.setAttribute('data-menu-state', menuState === 'closed' ? 'open' : 'closed');
+    console.log(menuState);
+
+    if(!menuState) header.setAttribute('data-menu-state', hasToggled ? 'closed' : 'open');
+    else header.setAttribute('data-menu-state', menuState === 'closed' ? 'open' : 'closed');
+
+    hasToggled = !hasToggled;
 });
