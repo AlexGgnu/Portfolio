a2fd076af156a2cf410b865f642d0d578efc4fe9
Alexis Gagneau <alexis.gagneau@icloud.com>
2026-04-17 14:22:06 +0000

    fix: Animation of toogle menu button

--- public/scripts/menu-toggle.js
+++ public/scripts/menu-toggle.js
@@ -1,13 +1,12 @@
 const menuToggleButton = document.getElementById('menu-toggle');
 const header = document.querySelector('header');
-let hasToggled = false;
+let isToogle = false;
 
 menuToggleButton.addEventListener('click', () => {
     const menuState = header.getAttribute('data-menu-state');
-    console.log(menuState);
 
-    if(!menuState) header.setAttribute('data-menu-state', hasToggled ? 'closed' : 'open');
+    if(!menuState) header.setAttribute('data-menu-state', !isToogle ? 'open' : 'closed');
     else header.setAttribute('data-menu-state', menuState === 'closed' ? 'open' : 'closed');
-
-    hasToggled = !hasToggled;
+    
+    isToogle = !isToogle;
 });
