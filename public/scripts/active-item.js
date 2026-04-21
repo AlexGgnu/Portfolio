7e91f29ade43de34c98788e97b3f3c27618c4dcb
Alexis Gagneau <alexis.gagneau@icloud.com>
2026-04-21 15:30:47 +0000

    feat: Update href for Home button

--- public/scripts/active-item.js
+++ public/scripts/active-item.js
@@ -1,13 +1,15 @@
 let navItems = document.querySelectorAll('#nav-items .item');
 
 function setActiveItem() {
-    const currentPath = window.location.hash || '/';
+    const currentPath = window.location.hash === "" ? "#hero" : window.location.hash;
 
     navItems.forEach(item => {
         const itemPath = item.querySelector('a').getAttribute('href');
 
-        if (itemPath === currentPath) item.setAttribute('data-active-item', 'true');
-        else item.removeAttribute('data-active-item');
+        if (itemPath === currentPath) {
+            item.setAttribute('data-active-item', 'true');
+            if (window.innerWidth < 768) closeMenu();
+        } else item.removeAttribute('data-active-item');
     });
 }
 
